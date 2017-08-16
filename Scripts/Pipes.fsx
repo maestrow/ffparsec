c8199baf51eb92f18dd16903f935acd202f8ebbf
maestrow <andrew.shmelev@gmail.com>
2017-08-16 17:25:44 +0300

    Pipes.fsx - demo of (+) operator without annotation

--- Scripts/Pipes.fsx
+++ Scripts/Pipes.fsx
@@ -11,8 +11,8 @@ open Parsec.Visualizers
 open Parsec.Pipes
 
 
-let p1 : Parser<char, CapturedResult<int>, unit> = returnP (1, true)
-let p2 : Parser<char, int, unit> = returnP 1
+let p1 = returnP (1, true)
+let p2 = returnP 1
 let p = p1 + p2
 
 let r = run p "dfg"
