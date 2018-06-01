047b1cde9c66cf9e499e569d49005105f903a25f
maestrow <andrew.shmelev@gmail.com>
2018-06-01 21:48:26 +0300

    update packages (paket update)

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 dotnet clean .\src\Parsec.fsproj
-dotnet clean .\tests\Tests.fsproj
 dotnet restore .\src\Parsec.fsproj
 dotnet build .\src\Parsec.fsproj
+
+dotnet clean .\tests\Tests.fsproj
 dotnet restore .\tests\Tests.fsproj
 dotnet build .\tests\Tests.fsproj
